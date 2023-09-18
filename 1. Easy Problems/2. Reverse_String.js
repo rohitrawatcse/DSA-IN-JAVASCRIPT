b45f687322841898661d46ed364119623ffc5245
Rohit Rawat <75693046+rohitrawatcse@users.noreply.github.com>
2023-09-19 00:45:30 +0530

    Added Reverse String Prroblem

---
+++ 1. Easy Problems/2. Reverse_String.js
@@ -0,0 +1,29 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ * 
+ * 
+ * Complexity
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ * 
+ */
+var reverseString = function(s) {
+
+    let left = 0;
+    let right = s.length - 1;
+    while(left <= right){
+
+        let temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        left++;
+        right--;
+
+    }
+
+    return s;
+    
+};
+
