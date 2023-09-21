b0dde24f6a728ab149a1c858a8829a3abbf4c503
Rohit Rawat <75693046+rohitrawatcse@users.noreply.github.com>
2023-09-22 01:07:47 +0530

    mergeAlternately Added

---
+++ 1. Easy Problems/7. mergeAlternately.js
@@ -0,0 +1,41 @@
+// mergeAlternately
+
+
+
+
+
+
+
+
+
+
+
+// In Java
+
+// class Solution {
+//     public String mergeAlternately(String word1, String word2) {
+
+//         int len1 = word1.length();
+//         int len2 = word2.length();
+//         int i = 0, j = 0;
+//         StringBuilder result = new StringBuilder();
+
+//         while (i < len1 && j < len2) {
+//             result.append(word1.charAt(i++));
+//             result.append(word2.charAt(j++));
+//         }
+
+//         // Append any remaining characters from word1
+//         while (i < len1) {
+//             result.append(word1.charAt(i++));
+//         }
+
+//         // Append any remaining characters from word2
+//         while (j < len2) {
+//             result.append(word2.charAt(j++));
+//         }
+
+//         return result.toString();
+        
+//     }
+// }
